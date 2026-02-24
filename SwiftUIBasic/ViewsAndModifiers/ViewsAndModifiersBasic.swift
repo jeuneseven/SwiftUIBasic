c665d36c291a8e6dd62eed05fa414fb3596342cc
jeuneseven <jeuneseven@gmail.com>
2026-02-24 16:16:03 +1300

    update

--- SwiftUIBasic/ViewsAndModifiers/ViewsAndModifiersBasic.swift
+++ SwiftUIBasic/ViewsAndModifiers/ViewsAndModifiersBasic.swift
@@ -7,26 +7,7 @@
 
 import SwiftUI
 
-struct Watermark: ViewModifier {
-    var text: String // custom view modifier can has their own property
 
-    func body(content: Content) -> some View {
-        ZStack(alignment: .bottomTrailing) {
-            content
-            Text(text)
-                .font(.caption)
-                .foregroundStyle(.white)
-                .padding(5)
-                .background(.black)
-        }
-    }
-}
-
-extension View { // extension a view can not have their own property
-    func watermarked(with text: String) -> some View {
-        modifier(Watermark(text: text))
-    }
-}
 
 struct ViewsAndModifiersBasic: View {
     var body: some View {
